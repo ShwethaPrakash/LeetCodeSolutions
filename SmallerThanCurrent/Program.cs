73687df47de93baec6ad163dc0079053018b6f30
Shwetha Prakash <shwetha.prakash@gmail.com>
2020-06-21 13:00:14 -0700

    Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
    Return the answer in an array.

---
+++ SmallerThanCurrent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallerThanCurrent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 8, 1, 2, 2, 3 };
+
+            int[] output = SmallerNumbersThanCurrent(nums);
+        }
+
+        public static int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            List<int> array = nums.ToList();
+            array.Sort();
+
+            int[] output = new int[nums.Length];
+
+            for (int i=0;i<nums.Length;i++)
+            {
+                output[i] = array.IndexOf(nums[i]);
+            }
+
+            return output;
+            
+        }
+
+        
+    }   
+}
